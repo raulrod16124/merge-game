d7c2433f8d48e939569f13acf06f9e56ba07f076
raul rodriguez <raul.rod16124@gmail.com>
2025-11-16 16:31:51 +0000

    implement PWA configuration

--- src/ui/constants/cosmicData.ts
+++ src/ui/constants/cosmicData.ts
@@ -13,14 +13,14 @@ export type CosmicType =
   | 'fragment_dark';
 
 export const COSMIC_ICONS: Record<CosmicType, string> = {
-  dust: '/src/assets/cosmic/dust.png',
-  micro_asteroid: '/src/assets/cosmic/micro_asteroid.png',
-  meteorite: '/src/assets/cosmic/meteorite.png',
-  baby_planet: '/src/assets/cosmic/baby_planet.png',
-  mature_planet: '/src/assets/cosmic/mature_planet.png',
-  star: '/src/assets/cosmic/star.png',
-  star_system: '/src/assets/cosmic/star_system.png',
-  nebula: '/src/assets/cosmic/nebula.png',
-  galaxy: '/src/assets/cosmic/galaxy.png',
-  fragment_dark: '/src/assets/cosmic/fragment_dark.png',
+  dust: '/cosmic/dust.png',
+  micro_asteroid: '/cosmic/micro_asteroid.png',
+  meteorite: '/cosmic/meteorite.png',
+  baby_planet: '/cosmic/baby_planet.png',
+  mature_planet: '/cosmic/mature_planet.png',
+  star: '/cosmic/star.png',
+  star_system: '/cosmic/star_system.png',
+  nebula: '/cosmic/nebula.png',
+  galaxy: '/cosmic/galaxy.png',
+  fragment_dark: '/cosmic/fragment_dark.png',
 };
